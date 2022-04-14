98f0775467b542cd4406c57a1707b623e9ebb5db
Cooper Patton <cpatton@irongalaxystudios.com>
2022-04-13 20:11:32 -0500

    Added UMG package dependency. Added WidgetComponent for later. Setup proximity trigger to hook into on overlap events

--- LookBasedPopups/Source/LookBasedPopups/ZC/ZCInteractable.cpp
+++ LookBasedPopups/Source/LookBasedPopups/ZC/ZCInteractable.cpp
@@ -1,6 +1,9 @@
 #include "LookBasedPopups/ZC/ZCInteractable.h"
 
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+DEFINE_LOG_CATEGORY(ZCInteractableLog)
 
 // Sets default values
 AZCInteractable::AZCInteractable()
@@ -11,7 +14,25 @@ AZCInteractable::AZCInteractable()
 	if (ProximityTrigger == nullptr)
 	{
 		ProximityTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("ProximityTrigger"));
+		
+		// Set debug sphere visible in game
+		ProximityTrigger->bHiddenInGame = false;
+
+		// Specify we only want to check overlap and  query collision
+		ProximityTrigger->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+		ProximityTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	
+		// Attach it to the root
 		SetRootComponent(ProximityTrigger);
+	}
+
+	if (PopupWidget == nullptr)
+	{
+		PopupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PopupWidget"));
+		if (PopupWidget)
+		{
+			PopupWidget->SetupAttachment(GetRootComponent());
+		}
 	}
 }
 
@@ -20,6 +41,27 @@ void AZCInteractable::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Subscribe for overlap event begin
+	if (!ProximityTrigger->OnComponentBeginOverlap.IsBound())
+	{
+		ProximityTrigger->OnComponentBeginOverlap.AddDynamic(this, &AZCInteractable::OnBeginOverlap);
+	}
+
+	// Subscribe for overlap event end
+	if (!ProximityTrigger->OnComponentEndOverlap.IsBound())
+	{
+		ProximityTrigger->OnComponentEndOverlap.AddDynamic(this, &AZCInteractable::OnEndOverlap);
+	}
+}
+
+void AZCInteractable::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(ZCInteractableLog, Log, TEXT("Began overlapping actor [%d]"), OtherActor ? OtherActor->GetUniqueID() : -1);
+}
+
+void AZCInteractable::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(ZCInteractableLog, Log, TEXT("Stopped overlapping actor [%d]"), OtherActor ? OtherActor->GetUniqueID() : -1);
 }
 
 // Called every frame
